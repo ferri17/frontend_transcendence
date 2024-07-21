bd0eff9f5bf43594945f712f89a3e3949f43b498
Ferran <ferranbosch17@gmail.com>
2024-07-22 01:50:16 +0200

    login page

--- src/js/main2.js
+++ src/js/main2.js
@@ -1,14 +1,14 @@
-import Lottie from "lottie-web";
-import '/src/styles/style.css';
+import Lottie from 'lottie-web';
+//import '/src/styles/style.css';
 import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '/src/styles/custom-bootstrap.css';
 
-"use strict";
+'use strict';
 
-  Lottie.loadAnimation({
+Lottie.loadAnimation({
 	container: document.getElementById('paddle-animation'), // the dom element that will contain the animation
 	renderer: 'svg',
 	loop: true,
 	autoplay: true,
 	path: '/src/assets/paddle_lottie.json' // the path to the animation json
-  });
+});
