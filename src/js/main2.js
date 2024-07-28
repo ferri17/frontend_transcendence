06442f7178048d7a8dba03ba9e80019680f49426
Ferran <ferranbosch17@gmail.com>
2024-07-29 01:13:04 +0200

    draft light mode toggle

--- src/js/main2.js
+++ src/js/main2.js
@@ -5,6 +5,13 @@ import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 'use strict';
 
+const	lightModeSwtich = document.querySelector('#light-mode-switch');
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+	updateLightMode();
+});
+
 Lottie.loadAnimation({
 	container: document.getElementById('paddle-animation'), // the dom element that will contain the animation
 	renderer: 'svg',
@@ -12,3 +19,17 @@ Lottie.loadAnimation({
 	autoplay: true,
 	path: '/src/assets/paddle_lottie.json'
 });
+
+// Light mode switch behaviour
+
+
+lightModeSwtich.addEventListener('change', (event) => updateLightMode(event));
+
+function	updateLightMode(event) {
+	if (event.target.checked)
+		document.documentElement.setAttribute('data-bs-theme', 'light');
+	else
+		document.documentElement.removeAttribute('data-bs-theme');
+
+}
+	
